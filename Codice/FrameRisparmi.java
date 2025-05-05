d2f69a44bfeb075323370a82b112131bbdebdaf1
ManueliK2000 <mp7sc75dp67@gmail.com>
2025-05-05 13:18:10 +0200

    Correggi errori di battitura e migliora la leggibilità nel FrameRisparmi.java

--- Codice/FrameRisparmi.java
+++ Codice/FrameRisparmi.java
@@ -1,5 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class FrameRisparmi {
    public FrameRisparmi() {
@@ -33,16 +35,16 @@ public class FrameRisparmi {
       btnImage.setBorderPainted(false);
       btnImage.setContentAreaFilled(false);
 
-      JLabel labelRisparmi = new JLabel("Risparmi");
+      JLabel labelRisparmii = new JLabel("Risparmi");
 
       JTextField textFieldRisparmi = new JTextField("0 €");
       JTextField textFieldConto = new JTextField("1000 €"); // Valore iniziale del conto
 
       barraSuperiore.add(btnPlus, BorderLayout.WEST);
       barraSuperiore.add(btnImage, BorderLayout.EAST);
-      labelRisparmi.setHorizontalAlignment(SwingConstants.CENTER);
-      labelRisparmi.setFont(new Font("Arial", Font.BOLD, 18));
-      barraSuperiore.add(labelRisparmi, BorderLayout.CENTER);
+      labelRisparmii.setHorizontalAlignment(SwingConstants.CENTER);
+      labelRisparmii.setFont(new Font("Arial", Font.BOLD, 18));
+      barraSuperiore.add(labelRisparmii, BorderLayout.CENTER);
 
       // Creazione del pannello principale
       JPanel panelRisparmi = new JPanel();
@@ -83,7 +85,7 @@ public class FrameRisparmi {
 
          // Cambia lo sfondo e il colore del testo
          barraSuperiore.setBackground(Color.BLACK);
-         labelRisparmi.setForeground(Color.WHITE);
+         labelRisparmio.setForeground(Color.WHITE);
          labelConto.setForeground(Color.WHITE);
          labelRisparmio.setForeground(Color.WHITE);
          textFieldRisparmi.setBackground(Color.BLACK);
@@ -97,7 +99,7 @@ public class FrameRisparmi {
 
          // Ripristina lo sfondo e il colore del testo
          barraSuperiore.setBackground(Color.WHITE);
-         labelRisparmi.setForeground(Color.BLACK);
+         labelRisparmio.setForeground(Color.BLACK);
          labelConto.setForeground(Color.BLACK);
          labelRisparmio.setForeground(Color.BLACK);
          textFieldRisparmi.setBackground(Color.WHITE);
@@ -177,8 +179,8 @@ public class FrameRisparmi {
       });
    }
 
-   // public static void main(String[] args) {
-   //    new FrameRisparmi();
-   // }
+   public static void main(String[] args) {
+      new FrameRisparmi();
+   }
 }
 
