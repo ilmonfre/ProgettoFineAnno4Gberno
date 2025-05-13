77b7c60ec1c3145e005ea253fe9ea1639320a339
LolloFrasco <frascogna.lorenzo29@gmail.com>
2025-05-13 11:40:16 +0200

    Add files via upload

---
+++ Codice/FrameAccount.java
@@ -0,0 +1,122 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.IOException;
+
+public class FrameAccount {
+    
+    JFrame frame;
+    JPanel topPanel, midPanel, bottomPanel;
+    JLabel topLabel;
+
+    JPanel panel1, btnPanel;
+    JLabel lblNomeTitolare, lblCognomeTitolare, lblCodFis, lblNtel, lblDataNascita, lblLuogoNascita, lblIndirizzo, lblCitta, lblEmail, lblPassword;
+
+    JButton btn;
+
+    Font fontIniziale, fontMid;
+
+    static Utente utente;
+
+    FrameAccount(){
+
+        frame = new JFrame();
+        frame.setTitle("Zaphyra Bank - Account");
+        frame.setLayout(new BorderLayout());
+
+        // per settare il frame in centro
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        int x = (screenSize.width - 500) / 2;
+        int y = (screenSize.height - 550) / 2;
+        frame.setLocation(x, y);
+
+        // top panel
+        topPanel = new JPanel();
+        topLabel = new JLabel("Il tuo account!");
+        fontIniziale = new Font("Serif", Font.BOLD, 28);
+        topLabel.setFont(fontIniziale);
+        topPanel.add(topLabel);
+        topPanel.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
+
+        frame.add(topPanel, BorderLayout.NORTH);
+
+        // mid panel
+        midPanel = new JPanel();
+        fontMid = new Font("Segoe UI", Font.PLAIN, 14);
+        midPanel.setBorder(BorderFactory.createEmptyBorder(30, 20, 50, 20));
+
+        //panel1 = new JPanel(new GridLayout(5, 2, 10, 20));
+
+        lblNomeTitolare = new JLabel("Nome: " + utente.getNome());
+        lblNomeTitolare.setFont(fontMid);
+
+        lblCognomeTitolare = new JLabel("Cognome: " + utente.getCognome());
+        lblCognomeTitolare.setFont(fontMid);
+
+        lblCodFis = new JLabel("Codice fiscale: " + utente.getCodFis());
+        lblCodFis.setFont(fontMid);
+        
+        lblNtel = new JLabel("Numero di telefono: " + utente.getnTel());
+        lblNtel.setFont(fontMid);
+
+        lblDataNascita = new JLabel("Data di nascita: " + utente.getDataNascita());
+        lblDataNascita.setFont(fontMid);
+
+        lblLuogoNascita = new JLabel("Luogo di nascita: " + utente.getLuogoNascita());
+        lblLuogoNascita.setFont(fontMid);
+
+        lblIndirizzo = new JLabel("Indirizzo di residenza: " + utente.getIndirizzo());
+        lblIndirizzo.setFont(fontMid);
+
+        lblCitta = new JLabel("Citta di residenza: " + utente.getCitta());
+        lblCitta.setFont(fontMid);
+
+        lblEmail = new JLabel("Email: " + utente.getEmail());
+        lblEmail.setFont(fontMid);
+
+        lblPassword = new JLabel("Password: " + utente.getPassword());
+        lblPassword.setFont(fontMid);
+        
+        panel1.add(lblNomeTitolare);
+        panel1.add(lblCognomeTitolare);
+        panel1.add(lblCodFis);
+        panel1.add(lblNtel);
+        panel1.add(lblDataNascita);
+        panel1.add(lblLuogoNascita);
+        panel1.add(lblIndirizzo);
+        panel1.add(lblCitta);
+        panel1.add(lblEmail);
+        panel1.add(lblPassword);
+        panel1.add(btn);
+        midPanel.add(panel1); 
+
+        // panel per un bottone
+        btn = new JButton("Dati della carta");
+        btn.setBackground(Color.decode("#5299D5"));
+        Dimension d = new Dimension(200, 35);
+        btn.setPreferredSize(d);
+
+        btn.setHorizontalAlignment(JButton.CENTER);
+        btn.setBorderPainted(false);
+        btn.setContentAreaFilled(false);
+
+        btn.addActionListener(new ActionListener (){
+            @Override
+            public void actionPerformed (ActionEvent e){
+
+                new FrameDatiCarta();
+
+            }
+        
+        });
+        midPanel.add(btn);
+
+        frame.add(midPanel, BorderLayout.CENTER);
+
+
+        frame.setSize(600, 550);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+}
---
+++ Codice/FrameDatiCarta.java
@@ -0,0 +1,102 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class FrameDatiCarta {
+    
+
+    JFrame frame, previousFrame;
+    JPanel topPanel, midPanel;
+    JLabel topLabel;
+    JButton btn;
+
+    JLabel lblIban, lblNcarta, lblDataScadenza;
+    JPasswordField lblcvv, lblpin;
+
+    static DatiCartaUtente dati;
+
+    Font fontIniziale, fontMid,fontBtn;
+
+    FrameDatiCarta(){
+
+        frame = new JFrame();
+        frame.setTitle("Zaphyra Bank - Dati carta");
+        frame.setLayout(new BorderLayout());
+
+        // per settare il frame in centro
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        int x = (screenSize.width - 500) / 2;
+        int y = (screenSize.height - 550) / 2;
+        frame.setLocation(x, y);
+
+
+        // top panel
+        topPanel = new JPanel();
+        topLabel = new JLabel("ILa tua carta!");
+        fontIniziale = new Font("Serif", Font.BOLD, 28);
+        topLabel.setFont(fontIniziale);
+        topPanel.add(topLabel);
+        topPanel.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
+
+        frame.add(topPanel, BorderLayout.NORTH);
+
+        // pannello per gestire i dati della carta
+        midPanel = new JPanel(new GridLayout(7, 1, 10, 20));
+        fontMid = new Font("Segoe UI", Font.PLAIN, 14);
+        midPanel.setBorder(BorderFactory.createEmptyBorder(30, 20, 50, 20));
+
+        lblIban = new JLabel("Iban: " + dati.getIBAN());
+        lblIban.setFont(fontMid);
+
+        lblNcarta = new JLabel("Numero della carta: " + dati.getnCarta());
+        lblNcarta.setFont(fontMid);
+
+        lblDataScadenza = new JLabel("Data di scadenza: " + dati.getDataScadenza());
+        lblDataScadenza.setFont(fontMid);
+
+        lblcvv = new JPasswordField("CVV: " + dati.getCvv());
+        lblcvv.setFont(fontMid);
+
+        lblpin = new JPasswordField("PIN: " + dati.getPin());
+        lblpin.setFont(fontMid);
+
+        midPanel.add(lblIban);
+        midPanel.add(lblNcarta);
+        midPanel.add(lblDataScadenza);
+        midPanel.add(lblcvv);
+        midPanel.add(lblpin);
+        
+        btn = new JButton("Indietro");
+
+        btn.setBackground(Color.LIGHT_GRAY);
+        Dimension d = new Dimension(200, 35);
+        btn.setPreferredSize(d);
+
+        fontBtn = new Font("Segoe UI", Font.BOLD, 14);
+        btn.setFont(fontBtn);
+        btn.setFont(fontBtn);
+
+        btn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frame.dispose();
+                
+                previousFrame.setVisible(true);   
+            }
+        });
+
+        midPanel.add(btn);
+
+        frame.add(midPanel, BorderLayout.CENTER);
+
+
+
+        
+
+        
+
+        frame.setSize(500, 550);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
